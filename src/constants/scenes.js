986a8cc75fd63188ee39153c8887b7e0dad2a14e
Nic Gordon <gordonna@gmail.com>
2018-06-05 09:18:37 +1000

    Add all scenes

--- src/constants/scenes.js
+++ src/constants/scenes.js
@@ -1,5 +1,16 @@
 const scenes = {
-  OUTSIDE_OFFICE_EAST: 'outside-office-east',
+  CARPARK: 'carpark',
+  COMMON_AREA: 'common-area',
+  CRAIGS_HOUSE: 'craigs-house',
+  DARK_SIDE: 'dark-side',
+  EASEYS: 'easeys',
+  FOYER: 'foyer',
+  GARAGE: 'garage',
+  KITCHEN: 'kitchen',
+  LIGHT_SIDE: 'light-side',
+  OUTSIDE_OFFICE: 'outside-office',
+  PATIO: 'patio',
+  TOILET: 'toilet',
 };
 
 export default scenes;
