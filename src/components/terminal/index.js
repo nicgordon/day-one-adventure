297dacb098b5b8c0fb3c206ffde7c7b940af7ee8
Nic Gordon <gordonna@gmail.com>
2018-06-05 21:29:28 +1000

    Always scroll to bottom when a command is sent

--- src/components/terminal/index.js
+++ src/components/terminal/index.js
@@ -1,18 +1,32 @@
 import { connect } from 'react-redux';
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import TerminalInput from '../terminal-input';
 
 import './styles.css';
 
-const Terminal = ({ log }) => (
-  <div className="terminalContainer">
-    <div className="terminal">
-      <div dangerouslySetInnerHTML={{ __html: log }} />
-      <TerminalInput />
-    </div>
-  </div>
-);
+class Terminal extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.container = React.createRef();
+  }
+
+  componentDidUpdate() {
+    this.container.current.scrollTop = this.container.current.scrollHeight;
+  }
+
+  render() {
+    const { log } = this.props;
+    return (
+      <div className="terminalContainer" ref={this.container}>
+        <div className="terminal">
+          <div dangerouslySetInnerHTML={{ __html: log }} />
+          <TerminalInput />
+        </div>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   log: state.present.game.log,
