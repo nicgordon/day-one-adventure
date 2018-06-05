b57bafa382ee99955ba7946100ada8399d13839e
Nic Gordon <gordonna@gmail.com>
2018-06-06 09:11:58 +1000

    Update meowing interaction

--- src/scenes/permanent/interactions/meow.js
+++ src/scenes/permanent/interactions/meow.js
@@ -4,6 +4,8 @@ import gameActions from '../../../store/actions/game';
 export default {
   pattern: new RegExp(`^(${constants.WORD_GROUP.VERB.MEOW})`),
   action: (state, dispatch) => {
-    dispatch(gameActions.pushMessage('You’re fired. Game over.'));
+    dispatch(
+      gameActions.pushMessage('You’re fired. Game over.<br /><br />...Okay, not really, but DON’T do that again.')
+    );
   },
 };
