5b59ffa0cbde6454ba1a08d6d267ae3a53d3df01
Nic Gordon <gordonna@gmail.com>
2018-06-06 06:20:19 +1000

    Update copy

--- src/scenes/outside-office/interactions/wait.js
+++ src/scenes/outside-office/interactions/wait.js
@@ -6,7 +6,7 @@ export default {
   pattern: new RegExp(`^(${constants.WORD_GROUP.VERB.WAIT})`),
   action: (state, dispatch) => {
     dispatch(
-      gameActions.pushMessage('You find a comfy nook and wait until someone shows up and opens the roller door.')
+      gameActions.pushMessage('You find a comfy nook and wait. Finally someone shows up and opens the roller door.')
     );
     dispatch(
       sceneActions.setFlag(constants.SCENE.OUTSIDE_OFFICE, {
