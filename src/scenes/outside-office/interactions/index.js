b55499d14956fa8b9eee8eac287d6a811ac473b4
Nic Gordon <gordonna@gmail.com>
2018-06-06 06:20:14 +1000

    Add todo

--- src/scenes/outside-office/interactions/index.js
+++ src/scenes/outside-office/interactions/index.js
@@ -4,4 +4,7 @@ import openRollerDoor from './open-roller-door';
 import ringDoorbell from './ring-doorbell';
 import wait from './wait';
 
+// @TODO:
+// knock (both roller door and glass door versions)
+
 export default [openRollerDoor, moveWest, moveNorth, wait, ringDoorbell];
