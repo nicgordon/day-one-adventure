2d823957e2955bdcbc260f6b32881f818894b620
Nic Gordon <gordonna@gmail.com>
2018-06-07 07:52:34 +1000

    Add standard scene help

---
+++ src/scenes/post-scene/interactions/help.js
@@ -0,0 +1,14 @@
+import gameActions from '../../../store/actions/game';
+
+export default {
+  pattern: new RegExp(`^help$`),
+  action: (state, dispatch) => {
+    dispatch(
+      gameActions.pushMessage(`Some things you could always try doing are:<br />
+        &gt; <em>describe scene</em> - To describe your current surroundings<br />
+        &gt; <em>move (in a compass direction)</em> - To walk somewhere else<br />
+        &gt; <em>take (thing)</em> - To put said thing into your inventory<br />
+        &gt; <em>inventory</em> - To check out your loot`)
+    );
+  },
+};
--- src/scenes/post-scene/interactions/index.js
+++ src/scenes/post-scene/interactions/index.js
@@ -1,7 +1,8 @@
 import describe from './describe';
 import describeScene from './describe-scene';
+import help from './help';
 import meow from './meow';
 import move from './move';
 import takeItem from './take-item';
 
-export default [describeScene, describe, move, takeItem, meow];
+export default [help, describeScene, describe, move, takeItem, meow];
