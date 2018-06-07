a43ab53b7db2e86256094d67fa44e75be287b89a
Nic Gordon <gordonna@gmail.com>
2018-06-07 21:27:09 +1000

    Update waiting action

--- src/scenes/outside-office/interactions/wait.js
+++ src/scenes/outside-office/interactions/wait.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import constants from '../../../constants';
 import gameActions from '../../../store/actions/game';
 import sceneActions from '../../../store/actions/scene';
@@ -5,8 +6,13 @@ import sceneActions from '../../../store/actions/scene';
 export default {
   pattern: new RegExp(`^(${constants.VERB.WAIT})`),
   action: (state, dispatch) => {
+    const isRollerDoorOpen = _.get(state, `present.scenes[${constants.SCENE.OUTSIDE_OFFICE}].rollerDoorOpen`, false);
     dispatch(
-      gameActions.pushMessage('You find a comfy nook and wait. Finally someone shows up and opens the roller door.')
+      gameActions.pushMessage(
+        isRollerDoorOpen
+          ? 'You’ve probably been waiting around for long enough now. Time to get on with it.'
+          : 'You find a comfy nook and wait. Finally someone shows up and opens the roller door.'
+      )
     );
     dispatch(
       sceneActions.setFlag(constants.SCENE.OUTSIDE_OFFICE, {
