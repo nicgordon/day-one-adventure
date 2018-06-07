606227c09d3704298ed015b59bb96af843931653
Nic Gordon <gordonna@gmail.com>
2018-06-07 21:02:20 +1000

    Emphasise speech

--- src/scenes/common-area/interactions/talk-to-felicity.js
+++ src/scenes/common-area/interactions/talk-to-felicity.js
@@ -5,10 +5,10 @@ export default {
   pattern: new RegExp(`^(${constants.VERB.TALK}) (felicity|flic)$`),
   action: (state, dispatch) => {
     dispatch(
-      gameActions.pushMessage(`"Hey there!" she says, her face beaming with a smile,
-        "I hope your first day is going swimmingly.
+      gameActions.pushMessage(`<em>"Hey there!"</em> Felicity says, her face beaming with a smile,
+        <em>"I hope your first day is going swimmingly.
         At any point you feel like you’ve had enough for the day please come and let me know.
-        In fact, have you had enough now?"`)
+        In fact, have you had enough now?"</em>`)
     );
     dispatch(gameActions.toggleInteractiveMode(constants.INTERACTIVE_SCENE.END_GAME));
   },
