241034e10816cbc3c85ad95ec8323e996392a56b
Nic Gordon <gordonna@gmail.com>
2018-06-08 08:55:08 +1000

    Rename help development mission

--- src/missions/help-development.js
+++ src/missions/help-development.js
@@ -3,6 +3,6 @@ import _ from 'lodash';
 import constants from '../constants';
 
 export default {
-  name: 'Fix the code',
+  name: 'Help development',
   predicate: state => _.get(state, `present.scenes[${constants.SCENE.LIGHT_SIDE}].codeFixed`, false),
 };
--- src/missions/index.js
+++ src/missions/index.js
@@ -1,8 +1,8 @@
 import feedWill from './feed-will';
 import fetchToiletPaper from './fetch-toilet-paper';
 import findPuzzlePiece from './find-puzzle-piece';
-import fixCode from './fix-code';
 import helpDesign from './help-design';
+import helpDevelopment from './help-development';
 import silenceCoffeeMachine from './silence-coffee-machine';
 
-export default [feedWill, fetchToiletPaper, findPuzzlePiece, fixCode, helpDesign, silenceCoffeeMachine];
+export default [feedWill, fetchToiletPaper, findPuzzlePiece, helpDesign, helpDevelopment, silenceCoffeeMachine];
