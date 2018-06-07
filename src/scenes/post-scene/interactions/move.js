d4332e3eb7820b35908736cc4c8ce619980aaff0
Nic Gordon <gordonna@gmail.com>
2018-06-07 21:36:23 +1000

    Increase clarity of movement error message

--- src/scenes/post-scene/interactions/move.js
+++ src/scenes/post-scene/interactions/move.js
@@ -4,6 +4,6 @@ import gameActions from '../../../store/actions/game';
 export default {
   pattern: new RegExp(`^(${constants.VERB.MOVE})`),
   action: (state, dispatch) => {
-    dispatch(gameActions.pushMessage('You can’t go there just now.'));
+    dispatch(gameActions.pushMessage('You can’t go there.'));
   },
 };
