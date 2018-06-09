da71fc405a579ccd0c4effffda4f0b858d3a47e6
Nic Gordon <gordonna@gmail.com>
2018-06-09 10:40:02 +1000

    Remove React :scream:

--- src/index.js
+++ src/index.js
@@ -1,26 +1,75 @@
-import { Provider } from 'react-redux';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import _ from 'lodash';
+import { ActionCreators as undoActions } from 'redux-undo';
 
-import App from './components/app';
+import executeCommand from './utils/execute-command';
+import gameActions from './store/actions/game';
 import registerServiceWorker from './registerServiceWorker';
+import scenes from './scenes';
 import store from './store';
 
 import './styles/reset.css';
 import './styles/base.css';
+import './styles/main.css';
 
 console.info('Checking the source kinda ruins the fun don’t you think?');
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
-
+// Add document handlers
 document.addEventListener('keypress', () => {
   const input = document.getElementById('input');
   input.focus();
 });
 
+// Add component handlers
+const form = document.getElementsByClassName('form')[0];
+const input = document.getElementsByClassName('input')[0];
+const helpButton = document.getElementsByClassName('helpButton')[0];
+const undoButton = document.getElementsByClassName('undoButton')[0];
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const command = _(input.value)
+    .chain()
+    .replace(/(>|&lt;|&gt;)/gi, '')
+    .trim()
+    .toLower()
+    .value();
+
+  if (_.isEmpty(command)) {
+    return;
+  }
+  store.dispatch(gameActions.submitCommand(command));
+  executeCommand(command, store.getState(), store.dispatch);
+
+  // Reset input
+  input.value = '';
+});
+helpButton.addEventListener('click', () => {
+  const command = 'help';
+  store.dispatch(gameActions.submitCommand(command));
+  executeCommand(command, store.getState(), store.dispatch);
+});
+undoButton.addEventListener('click', () => {
+  store.dispatch(undoActions.undo());
+});
+
+// Add state subscriptions
+const locationName = document.getElementsByClassName('locationName')[0];
+const log = document.getElementsByClassName('log')[0];
+const terminalContainer = document.getElementsByClassName('terminalContainer')[0];
+
+let currentState;
+const subscribeListener = () => {
+  const previousState = currentState;
+  currentState = store.getState();
+  if (_.get(currentState, `present.game.log`) !== _.get(previousState, `present.game.log`)) {
+    log.innerHTML = _.get(currentState, `present.game.log`);
+    terminalContainer.scrollTop = terminalContainer.scrollHeight;
+  }
+  locationName.innerText = scenes[_.get(currentState, `present.user.location`)].name;
+};
+store.subscribe(subscribeListener);
+
 registerServiceWorker();
+
+// Basically, do the first render
+subscribeListener();
